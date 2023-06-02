ea6ba4d2cfee586b600ab1ee13f05c29a6dd64fb
Florian Strasser <56030947+flostrasser@users.noreply.github.com>
2023-06-02 21:47:13 +0200

    Remove airbnb config & prettier from eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,20 +4,23 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'airbnb-base',
-    'prettier',
-    'plugin:prettier/recommended', // should be last extension in array
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+    project: './tsconfig.json',
   },
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ['@typescript-eslint'],
   rules: {
-    'prettier/prettier': 'warn',
-    'no-param-reassign': 'off',
+    'no-param-reassign': ['error', { props: false }],
     'import/prefer-default-export': 'off',
     'import/extensions': 'off',
+    'no-shadow': 'off', // fixes bug with enums https://stackoverflow.com/questions/63961803/eslint-says-all-enums-in-typescript-app-are-already-declared-in-the-upper-scope
+    '@typescript-eslint/no-shadow': ['error'], // same as above
   },
+  ignorePatterns: ['.eslintrc.js'], // ignore this file
 };
